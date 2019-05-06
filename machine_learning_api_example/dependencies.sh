1e9a4eb9d27d99235c259911c67fd5d677b9b1a7
Mauricio Branbilla Junior <mbranbilla@outlook.com>
2019-05-06 17:50:39 -0300

    Api example (#4)
    * Create a gitignore file
    
    * Install system requirements and python packages
    
    * Create a API for machine learning prediction
    
    * Setup docker container
    
    * Create a simples logistic-regression model for titanic data
    
    * Add description and instructions
    
    * Fix dependencies and improove DockerFile

--- machine_learning_api_example/dependencies.sh
+++ machine_learning_api_example/dependencies.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 # Update the repositories
-apk update
+## apk update
 
 # Install dependencies
-
-##  apk add <package>
-##  apk add --no-cache --virtual build-dependencies
-
-# Remove the temporary dependencies
-apk del build-dependencies
+##  apt-get install <package>
